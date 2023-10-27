43d7020d74270b36144cc187fb648ae84ed63c93
Brian Kurilko <briankurilko@gmail.com>
2023-10-27 11:08:09 -0400

    first commit

---
+++ src/main/java/com/example/renee/ReneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.renee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReneeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/renee/ReneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReneeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
